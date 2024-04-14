4472dcfc8b186810319a1f8a132d4e0209e62be2
abfiker <new94abemelek@gmail.com>
2024-04-14 23:01:03 +0300

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+export default function FirstPost(){
+    return (
+        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href='/'>Back to home</Link>
+                </h2>
+                <Image 
+                    src='/images/abeman.jpg'
+                    alt='Abemelek'
+                    width={144}
+                    height={144}
+                />
+        </Layout>
+        </>
+    )
+}
